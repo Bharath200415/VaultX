d0a5acf0dc5c1af47754c029a713919d573e12eb
Ubuntu <ubuntu@ip-172-31-81-221.ec2.internal>
2026-03-27 05:42:43 +0000

    deploying on ec2

--- frontend/src/api/files.js
+++ frontend/src/api/files.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3000/api" });
+const API = axios.create({ baseURL: "/api" });
 
 export const uploadFile = (formData, onProgress) =>
   API.post("/files/upload", formData, {
